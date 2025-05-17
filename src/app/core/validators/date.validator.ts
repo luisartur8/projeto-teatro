145002c4919685423cf2058f6ef1a95359b7a528
Luis Artur <luisartur8@hotmail.com>
2025-05-17 20:03:38 -0300

    fix

--- src/app/core/validators/date.validator.ts
+++ src/app/core/validators/date.validator.ts
@@ -4,15 +4,13 @@ export class ApplicationDateValidators {
 
   static dateValidator(control: AbstractControl): ValidationErrors | null {
     const value = control.value;
-    let isValid = false;
 
-    console.log(value);
-    
+    let isValid = false;
 
     const [dia, mes, ano] = value.split('/');
 
     if (ano < new Date().getFullYear().toString() && ano >= '1900' && dia <= '31' && mes <= 12) {
-        isValid = true;
+      isValid = true;
     }
 
     return isValid ? null : { invalidDate: true }
