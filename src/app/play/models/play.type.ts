352e8dcdfe55fd9302ce86de0c8bd9fcdbf44ce8
Guilherme Gonçalves <guilhermeg0910@outlook.com>
2025-06-22 12:15:24 -0300

    componente peça integrado com o backend

--- src/app/play/models/play.type.ts
+++ src/app/play/models/play.type.ts
@@ -1,9 +1,29 @@
 export type Play = {
-  id?: number,
-  name: string,
-  image: string | null,
-  synopsis: string | null,
-  gender: string,
-  address: string,
-  capacity: number,
-}
+  id?: string;
+  name: string;
+  image: string | null;
+  synopsis: string | null;
+  gender: string;
+  address: string;
+  capacity: number;
+
+
+  directorId?: string;
+  theaterId?: string;
+  actorId?: string[];
+
+  director?: {
+    id: string;
+    name: string;
+  };
+
+  theater?: {
+    id: string;
+    name: string;
+  };
+
+  actor?: {
+    id: string;
+    name: string;
+  }[];
+};
